4c8e8ee3066ca001cc3efd2ac10306d5b01faffc
dansmaciell <dsmaciel2005@gmail.com>
2024-07-04 20:13:04 -0300

    Add files via upload

---
+++ site_teste/script.js
@@ -0,0 +1,7 @@
+function respostaSim() {
+    window.location.href = "https://www.youtube.com/watch?v=ebo1jNfIgA4&t=21s";
+}
+
+function respostaNao() {
+    alert("Não existe essa opção samarinha 😢");
+}
